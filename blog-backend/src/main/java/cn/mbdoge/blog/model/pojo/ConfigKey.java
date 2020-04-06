74cc98c0d8fa859dd45a9ec87252f1256552054c
jjjjyx <jyx@rpgame.net>
2020-04-06 23:15:45 +0800

    delete old file

--- blog-backend/src/main/java/cn/mbdoge/blog/model/pojo/ConfigKey.java
+++
@@ -1,9 +0,0 @@
-package cn.mbdoge.blog.model.pojo;
-
-public @interface ConfigKey {
-    String ENABLE_REGISTER = "enable.register";
-    String ENABLE_ADMIN_PAGE = "enable.admin.page";
-    String ENABLE_BATCH_PAGE = "enable.batch.page";
-    String ENABLE_UPDATE_FUN = "enable.update.user.fun";
-    String TEST_ACCOUNT_SEARCH_LIMIT = "try.day.limit";
-}
--- blog-backend/src/main/java/cn/mbdoge/blog/model/pojo/DataView.java
+++
@@ -1,8 +0,0 @@
-package cn.mbdoge.blog.model.pojo;
-
-/**
- * @author jyx
- */
-public class DataView {
-    public interface BaseView {}
-}
